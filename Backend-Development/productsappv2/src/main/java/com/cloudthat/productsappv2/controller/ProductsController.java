f631490c367cf9405c6d6ed2705bf535db557324
CT-vishwas <vishwas@cloudthat.com>
2024-11-06 17:03:59 +0530

    updated code

--- Backend-Development/productsappv2/src/main/java/com/cloudthat/productsappv2/controller/ProductsController.java
+++ Backend-Development/productsappv2/src/main/java/com/cloudthat/productsappv2/controller/ProductsController.java
@@ -1,13 +1,12 @@
 package com.cloudthat.productsappv2.controller;
 
 
-import com.cloudthat.productsappv2.entity.Product;
+
 import com.cloudthat.productsappv2.model.ApiResponse;
 import com.cloudthat.productsappv2.model.ProductModel;
 import com.cloudthat.productsappv2.model.ProductRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -61,7 +60,7 @@ public class ProductsController {
     public ResponseEntity<ApiResponse> getProducts(
         @RequestParam(defaultValue = "0") int page,
         @RequestParam(defaultValue = "5") int size,
-        @RequestParam(defaultValue = "product_name") String sort,
+        @RequestParam(defaultValue = "productName") String sort,
         @RequestParam(defaultValue = "asc") String direction
     ){
         Page<ProductModel> products = productService.getProducts(page, size, sort, direction);
